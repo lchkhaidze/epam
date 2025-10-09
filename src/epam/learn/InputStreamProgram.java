62fb6d23ee484dbcc70253b713a77c2cf25efa51
Levan Chkhaidze <levan@live.com>
2025-10-09 22:44:43 +0400

    add first commit to git

---
+++ src/epam/learn/InputStreamProgram.java
@@ -0,0 +1,11 @@
+package epam.learn;
+
+import java.util.Scanner;
+
+public class InputStreamProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        System.out.println("Hello " + input);
+    }
+}
